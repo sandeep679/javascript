8140bc2b1482ab01ec99b303e88f08ca61d30c1c
sandeep679 <118195440+sandeep679@users.noreply.github.com>
2024-05-13 14:44:07 +0000

    datatype

---
+++ basics/2datatypes.js
@@ -0,0 +1,24 @@
+"use strict" //trest all js code as newer version
+//alert(3+3) we are using nodejs,not browser
+console.log(3+3)
+console.log("hero");
+   
+let name = "sandeep"
+let age =18
+let isloggedin = false
+ 
+
+//datatype====
+//numer=> 2to power 53
+//bigint-- used for bigger number
+//string-- " "
+//boolean-- true/false
+//null-- standalone value
+//undefined-- value is not assign
+//symbol-- unique
+
+//object
+ 
+
+console.log(typeof age);//undefined
+console.log(typeof null);// gives object
