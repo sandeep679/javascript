b09c881e568d8a427be7af2921204e32ecc0a09a
sandeep679 <118195440+sandeep679@users.noreply.github.com>
2024-05-17 05:22:13 +0000

    memory

--- basics/datatypesummary.js
+++ basics/datatypesummary.js
@@ -3,20 +3,45 @@
 //# Primtive - it callback by value. 
 //it have 7 types : String,Number, Boolean, null, undefined ,symbol,BigInt
 
-const id= Symbol('123')
-const anotherid=Symbol('123')
-console.log(id===anotherid);
+// const id= Symbol('123')
+// const anotherid=Symbol('123')
+// console.log(id===anotherid);
 
 
 //Reference type(non-permitive)- it callback by reference
 // Array, Objects ,Functions
-const hero =["spderman","ironman","deadpool"]
+// const hero =["spderman","ironman","deadpool"]
 
-let obj={
-    name:"golu",
-    age:18
-}
+// let obj={
+//     name:"golu",
+//     age:18
+// }
 
-const myfunction= function(){
-    console.log("hello world!");
+// const myfunction= function(){
+//     console.log("hello world!");
+// }
+
+
+
+//******************************************************************************** */
+
+//stack and heap memory
+
+let myyoutubename="gogo"
+
+let anothernname= myyoutubename
+myyoutubename ="zeo"
+
+console.log(anothernname);
+console.log(myyoutubename);
+
+
+let user1 ={
+    email:"hero@google,com",
+    upi :"user@ybl"
+
 }
+let user2= user1
+user2.email="yoyo@google.com"
+console.log(user1.email);    //we get same value because value is changed
+console.log(user2.email);
