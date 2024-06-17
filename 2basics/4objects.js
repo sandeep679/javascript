86d3fb8c456d11204ba6dfcb8a6aa4cf729639e3
Sandeep <118195440+sandeep679@users.noreply.github.com>
2024-06-17 19:10:21 +0000

    object2

---
+++ 2basics/4objects.js
@@ -0,0 +1,70 @@
+
+//                   SINGLETOn
+
+//const tinderuser =newobject()     object declaration
+ const tinderuser ={}
+
+
+  tinderuser.id = "123abc"
+  tinderuser.name = "samay"
+  tinderuser.logged = false
+
+  //console.log(tinderuser);
+
+  const regularuser ={
+    email: "some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"samay",
+            lastname:"raina"
+        }
+    }
+  }
+
+  //console.log(regularuser.fullname.userfullname.firstname);
+
+
+  const obj={
+    1:"a",
+    2:"b"
+  }
+  const obj2={
+    3:"c",
+    4:"f"
+  }
+  const obj4={
+    5:"c",
+    6:"f"
+  }
+  //const obj3 = Object.assign({}, obj,obj2,obj4)    merge object
+
+  //merge object
+  const obj3 ={...obj,...obj2,...obj4}  
+
+  //console.log(obj3);
+
+
+
+//database value
+const user =[{
+    id:1,
+    email:"yoyo@gmail.com"
+},{
+    id:3,
+    email:"yo@gmail.com"
+},{
+    id:3,
+    email:"y@gmail.com"
+},
+]
+
+//console.log(user[1].email);
+
+console.log(tinderuser);
+
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+
+console.log(tinderuser.hasOwnProperty("logged"));
+
