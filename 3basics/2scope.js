921771b4fb4820e074745587764603de0754479f
Sandeep <118195440+sandeep679@users.noreply.github.com>
2024-06-19 18:38:56 +0000

    scope with nested function

--- 3basics/2scope.js
+++ 3basics/2scope.js
@@ -1,11 +1,52 @@
 
 
-if(true){
-    let a=10
-    const b=20
-    var c=30
+// if(true){
+//     let a=10
+//     const b=20
+//     var c=30
 
+// }
+// //console.log(a);
+// //console.log(b);
+// console.log(c);
+
+
+// nested function
+
+// function one(){
+
+//     const user = "samay"
+//     function two(){
+
+//         const website ="youtube"
+//         console.log(user);
+//     }
+//    // console.log(website)
+//     two();
+// }
+
+// one()
+
+// if(true){
+//     const username ="samay"
+//     if(username==="samay")    //// scope understanding
+//         {
+//             const website="youtube"
+//             console.log(username + website);
+       // }
+       // console.log(website);
+//}
+//console.log(username);
+
+
+//++++++++++++++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++
+
+// function addone(num){
+//     return num+1
+// }
+// addone(5)
+
+const addone=function(num){   /// ANOTHERWAY OF DECLARATION OF FUNCTION
+    return num+1
 }
-//console.log(a);
-//console.log(b);
-console.log(c);
+addone(5)
