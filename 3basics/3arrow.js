102091f598120179601f1ec54ac3bfc2def51d78
Sandeep <118195440+sandeep679@users.noreply.github.com>
2024-06-19 19:18:59 +0000

    arrrow

---
+++ 3basics/3arrow.js
@@ -0,0 +1,43 @@
+const user ={
+    username: "samay",
+    price:99,
+    welcomemessage:function(){
+        console.log(`${this.username},welcome to website`);
+        console.log(this);   // it show current context in object
+    }
+}
+// user.welcomemessage()
+// user.username="sam"
+// user.welcomemessage()
+
+//console.log(this); // it show empty in node but it show window in browser because window is global
+
+// function chai(){
+//     let username ="samay"
+//     console.log(this.username);
+// }
+// chai()
+
+
+                     ///   arrow function
+
+// const chai = ()=>
+// {
+//         let username ="samay"
+//         console.log(this.username);
+//     }
+//     chai()
+    
+//  const addtwo =(num1,num2)=>{
+//     return num1+num2
+
+//  }
+//  console.log(addtwo(3,4));
+
+
+//const addtwo =(num1,num2)=>  (num1+num2) // expilit return
+const addtwo =(num1,num2)=>  ({username:"samay"})  //expilit return of objects
+
+
+console.log(addtwo(3,4))
+
