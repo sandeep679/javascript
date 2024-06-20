0abbc3d8be4a8506d143af68b04d57c46bf93c2a
Sandeep <118195440+sandeep679@users.noreply.github.com>
2024-06-20 13:52:40 +0000

    if condition

---
+++ 4controlflow/one.js
@@ -0,0 +1,55 @@
+//if
+    
+
+// if(condition){
+
+
+        // }
+
+
+const temperature = 41
+
+// if(temperature <50){
+//     console.log("less than 50");
+//     }
+
+//     console.log("temperature is grester than 50");
+
+
+
+
+                      // if and else
+    // if(temperature <50){
+    //     console.log("less than 50");
+    //     }
+    //     else{
+    //         console.log("temperature is grester than 50");
+    //     }
+
+
+//     const score =200
+//     if(score>100){
+//         const power ="fly"
+//         console.log(`user power:${power}`);
+//     }
+//
+
+            // shorthand notation
+//if(balance>500) console.log("test");
+
+/// if and else ,else if
+
+
+  const userlogged =true
+   const debitcard =true
+        const googlein =true
+        const  emailin =true
+
+   if(userlogged && debitcard)
+        {
+                console.log("allow to buy");
+        }
+
+        if(googlein || emailin){
+                console.log("user logged in");
+        }
