27c9200d6568bda67769882b7bfc42aa24e89ed6
Sandeep <118195440+sandeep679@users.noreply.github.com>
2024-06-20 14:40:32 +0000

    switch

---
+++ 4controlflow/switch.js
@@ -0,0 +1,51 @@
+// switch statements
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month =3
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month ="march"
+
+switch (month) {
+    case "january":
+        console.log("january");
+        break;
+    case "feb" :
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        break;
+}
