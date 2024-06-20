b12af01a888c8a9452afbf74c212e7bd02e1a4ce
Sandeep <118195440+sandeep679@users.noreply.github.com>
2024-06-20 19:27:16 +0000

    for in

---
+++ 5iteration/four.js
@@ -0,0 +1,29 @@
+// const object ={   
+//     js:'javascript',
+//     cpp :'c++',
+//     rb:" ruby",
+//     swift :'swift by apple'
+// }
+
+//2 for in   -    used to loop in object
+
+// for (const key in object) {                //for printin key and value together
+//    console.log(`key ${key} shortcut for ${object[key]}`);
+//     }
+
+// const program =["js","rb","py","java"]
+// for (const key in program) {
+//     console.log(program[key]);
+//     }
+
+    const map = new Map()
+map.set('IN',"INDIA")
+map.set('UK',"UNITED kingdom")
+map.set('FR',"France")
+
+// for (const key in object) {
+//     console.log(key);
+   // }
+for (const key in object) {
+    console.log([key]);   //it show key value
+    }
